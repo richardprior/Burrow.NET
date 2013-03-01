3779694780b46298aaf43791db792a51aa96b192
Rich <richard.prior@priordevelopment.com>
2013-03-01 13:30:11 +0000

    Upgraded NuGet dependencies to latest versions (tests still pass):  -> Castle.Core 3.2.0  -> Newtonsoft.Json 4.5.11  -> RabbitMQ.Client 3.0.2  -> NSubstitute 1.5.0.0 Added solution-wide NuGet package restore

--- nuget/Pack.bat
+++ nuget/Pack.bat
@@ -3,8 +3,8 @@ del Burrow.*.nupkg
 SETLOCAL
 SET VERSION=0.0.1
 
-Nuget\nuget pack Burrow\Package.nuspec -Version %VERSION%
-Nuget\nuget pack Burrow.Extras\Package.nuspec -Version %VERSION%
-Nuget\nuget pack Burrow.RPC\Package.nuspec -Version %VERSION%
+..\src\.nuget\nuget pack Burrow\Package.nuspec -Version %VERSION%
+..\src\.nuget\nuget pack Burrow.Extras\Package.nuspec -Version %VERSION%
+..\src\.nuget\nuget pack Burrow.RPC\Package.nuspec -Version %VERSION%
 ENDLOCAL
 pause
